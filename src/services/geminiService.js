d1a0624a9a9de7d40dc65c8b35024e9bebf9960a
Vishnu220805 <144658747+Vishnu220805@users.noreply.github.com>
2025-04-01 15:33:36 +0530

    updated with the backend url

--- src/services/geminiService.js
+++ src/services/geminiService.js
@@ -1,6 +1,6 @@
 export const sendMessageToGemini = async (message) => {
     try {
-        const response = await fetch("http://localhost:5000/gemini", {
+        const response = await fetch("https://travel-buddy-backend-haw2.onrender.com/gemini", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
